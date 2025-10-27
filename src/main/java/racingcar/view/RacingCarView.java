80480d7091c6636bbb32c75b6729e46894f41c9c
minj-L <wonder_minji@naver.com>
2025-10-27 21:33:34 +0900

    fix: 출력 구분자 앞뒤로 공백 추가

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -4,7 +4,7 @@ import racingcar.common.ConsoleMessage;
 
 public class RacingCarView {
     public void carMovingResult(String car, String goResult) {
-        System.out.println(car + ":" + goResult);
+        System.out.println(car + " : " + goResult);
     }
 
     public void fianlWinnerResult(String winner) {
