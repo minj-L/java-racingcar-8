7f5fb93b939e5246c5cff9a4572c16d2a1d6df8b
minj-L <wonder_minji@naver.com>
2025-10-27 21:33:15 +0900

    refact(checkDelimiter), fix: 자동차 이름 및 구분자 판별 함수 수정, 메서드명 및 오류 로직 수정

--- src/main/java/racingcar/service/GamerManageService.java
+++ src/main/java/racingcar/service/GamerManageService.java
@@ -5,31 +5,41 @@ import static java.util.Objects.isNull;
 public class GamerManageService {
     public String[] splitCars(String inputCars) {
         checkIsNull(inputCars);
-        checkDelimiter(inputCars);
 
-        String cleanedInput = inputCars.replace(" ", "");
+        String cleanedInput = cleanInput(inputCars);
+
+        checkDelimiter(cleanedInput);
 
         String[] cars = cleanedInput.split(",");
-        checkCarNames(cars);
+        checkCarNamesLength(cars);
 
         return cars;
     }
 
     private void checkIsNull(String inputCars) {
         if (isNull(inputCars) || inputCars.isBlank()) {
-            throw new IllegalArgumentException("잘못된 입력 형식: 자동차 이름을 입력하지 않았습니다.");
+            throw new IllegalArgumentException(
+                    "잘못된 입력 형식: 자동차 이름을 입력하지 않았습니다.");
         }
     }
 
     private void checkDelimiter(String inputCars) {
-        if (!inputCars.contains(",")) {
-            throw new IllegalArgumentException("잘못된 입력 형식: 자동차 이름을 ','로 구분해주세요");
+        String[] cars = inputCars.split(",");
+        for (String car : cars) {
+            if (!car.matches("[가-힣a-zA-Z0-9]+")) {
+                throw new IllegalArgumentException(
+                        "잘못된 입력 형식: 자동차 이름은 한글/영문만 가능하며 ','로 구분해야 합니다.");
+            }
         }
     }
 
-    private void checkCarNames(String[] carNames) {
+    private String cleanInput(String inputCars) {
+        return inputCars.replace(" ", "");
+    }
+
+    private void checkCarNamesLength(String[] carNames) {
         for (String carName : carNames) {
-            if (carName.length() > 6) {
+            if (carName.length() > 5) {
                 throw new IllegalArgumentException(
                         "이름이 5자 초과된 차가 있어 경주를 시작 할 수 없습니다.: " + carName);
             }
